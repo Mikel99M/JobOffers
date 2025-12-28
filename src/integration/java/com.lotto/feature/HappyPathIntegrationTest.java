a7e5d72c1733ad8b6703c2ccb294d7f32e577ef2
Michał Mielcarek <mike249mielcarek@gmail.com>
2025-12-28 13:29:40 +0100

    setting up docker compose for mongo db. Setting outline for integration tests

---
+++ src/integration/java/com.lotto/feature/HappyPathIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.lotto.feature;
+
+import com.lotto.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class HappyPathIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    void f() throws Exception {
+
+
+    }
+}
---
+++ src/main/java/com/joboffers/infrastracture/login/LoginController.java
@@ -0,0 +1,4 @@
+package com.joboffers.infrastracture.login;
+
+class LoginController {
+}
